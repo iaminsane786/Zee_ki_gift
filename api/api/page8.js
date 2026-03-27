f4435ff7b7b35a433293a3617a101f0a28f2886c
iaminsane786 <oplegit786@gmail.com>
2026-03-28 01:33:59 +0530

    Update page8.js

--- api/api/page8.js
+++ api/api/page8.js
@@ -1,17 +1,1 @@
-export default async function handler(req, res) {
-  const token = "8701915082:AAFi4g7h2CDy9xqXOGfjY8iGJNW5WrodAf4";
-  const chatId = "1468507562";
 
-  await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: "Someone visited Page 8"
-    })
-  });
-
-  res.status(200).send("ok");
-}
